029c7cd12ae167314c7a9e129b731a6dae9f9a58
John-Gee <john.ettedgui@gmail.com>
2022-07-11 19:53:07 -0700

    Switched from zenity to yad

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 This is a little bash script to help masternode owners vote.
-Currently it only works with 1 masternode at a time, that can be upgraded later.
+Currently it only works with 1 masternode at a time, that could be enhanced later.
 
-This script depends on bash, defi-cli, sed, xclip and zenity; it assumes defid is already running.
+This script depends on bash, defi-cli, sed, xclip and yad; it assumes defid is already running.
 
 To use this, you will need to create copy of the template config as vote.conf, input in your masternode owner address and the path to the defi-cli binary; there are other optional settings in there.
 
-The title of the CFP/DFP is clickable and should launch the appropriate page in a browser.
+The title of the CFP/DFIP is clickable and should launch the appropriate page in a browser.
 After clicking the voting button, the dfi-cli result will be stored in the default clipboard, allowing for an easy paste on the page opened above.
--- vote.sh
+++ vote.sh
@@ -5,43 +5,51 @@ then
   source vote.conf
 fi
 
-cfps="cfps/2202.txt"
+cfps="cfps/2207.txt"
 
 if [ -z $owner ]
 then
-  owner=`/usr/bin/zenity --entry --title "Owner" --text "Please input your owner address"`
+  owner=`/usr/bin/yad --entry --title "Owner" --text "Please input your owner address"`
 fi
 
-password=`/usr/bin/zenity --password --title "Password"`
+password=`/usr/bin/yad --text-align=center --text "Please input your password" --entry --hide-text`
 
 if [ ! -z $password ]
 then
   $defiPath/defi-cli -conf="$conf" walletpassphrase "$password" $timeout
   if [ 0 != $? ]
   then
-    /usr/bin/zenity --error --no-wrap --text "Wallet unlocking failed!"
+    /usr/bin/yad --error --no-wrap --text "Wallet unlocking failed!"
     exit 1
   fi
   password=""
 fi
 
+i=0
+num=0
 while read -r CFP
 do
-  cfpId=`echo $CFP | cut -d: -f1`
-  cfpTitle=`echo $CFP | cut -d: -f2 | cut -d] -f1`
-  cfpURL=`echo $CFP | cut -d] -f2`
-  cleanTitle=`echo $cfpTitle | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g'`
-  
-  vote=`/usr/bin/zenity --info --no-wrap --title "Vote" \
-        --text "<big><a href=\"$cfpURL\">$cleanTitle</a></big>" \
-        --ok-label yes \
-        --extra-button no --extra-button neutral`
-  if [ 0 == $? ]
+  if [ $i -eq 0 ]
   then
-    vote=yes
+    num=$CFP
+  else
+    cfpId=`echo $CFP | cut -d\| -f1`
+    cfpTitle=`echo $CFP | cut -d\| -f2`
+    cfpURL=`echo $CFP | cut -d\| -f3`
+    cleanTitle=`echo $cfpTitle | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g'`
+    
+    vote=`/usr/bin/yad --title "DFC Voter" --height=200 --list \
+          --text "<big><a href='$cfpURL'>$cleanTitle</a></big>" \
+          --column "Vote $i/$num" Yes Neutral No`
+    echo "vote: $vote"
+    if [ 0 == $? ]
+    then
+      vote=yes
+    fi
+    sign=`"$defiPath/defi-cli" -conf="$conf" signmessage "$owner" "$cfpId-$vote"`
+    echo "defi-cli signmessage $owner $cfpId-$vote $sign" | xclip -selection c
   fi
-  sign=`"$defiPath/defi-cli" -conf="$conf" signmessage "$owner" "$cfpId-$vote"`
-  echo "defi-cli signmessage $owner $cfpId-$vote $sign" | xclip -selection c
+  ((i++))
 done < "$cfps"
 
 "$defiPath/defi-cli" -conf="$conf" walletlock
